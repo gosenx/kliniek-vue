811eac34b8d1bdf60b78fa17d1635c21a135360d
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-04-15 14:42:14 +0200

    init project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+
+import './assets/css/main.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuex)
+Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
