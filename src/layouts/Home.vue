1c931bf533e70af70fc7fd76fd91574777d3419d
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-05-11 13:35:50 +0200

    adding home page

---
+++ src/layouts/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Home Page <br />
+    <router-link to="/example">Example</router-link> <br />
+    <router-link to="/register-patient">Register Patient</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
 import VueRouter from "vue-router";
+import Home from "./layouts/Home";
 import Example from "./components/Example";
 import RegisterPatient from "./layouts/patient/RegisterPatient";
 
@@ -6,6 +7,7 @@ export const router = new VueRouter({
   base: "/",
   mode: "history",
   routes: [
+    { path: "/", component: Home },
     { path: "/example", component: Example },
     { path: "/register-patient", component: RegisterPatient },
   ],
