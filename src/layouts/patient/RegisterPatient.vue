c07978836496e5a724f37765766f5583e9d56209
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-05-12 16:52:54 +0200

    wip: editing the UI of patient registration form

--- src/layouts/patient/RegisterPatient.vue
+++ src/layouts/patient/RegisterPatient.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex flex-col-reverse bg-gray-300 md:flex-row">
-    <div class="w-full h-screen bg-white p-5 md:p-10">
+    <div class="w-full flex flex-col justify-center h-screen bg-white p-5 md:p-10">
       <form action="" method="post">
         <div class="form-group">
-          <label class="label-normal" for="name">Name</label>
+          <label class="label-normal" for="name">Nome Completo</label>
           <input class="input-normal" placeholder="Carla Armindo Maposse" type="text" id="name" required />
         </div>
 
@@ -13,7 +13,7 @@
         </div>
 
         <div class="form-group">
-          <label class="label-normal" for="bi">Bilhete Identidade</label>
+          <label class="label-normal" for="bi">Bilhete de Identidade</label>
           <input class="input-normal" placeholder="110300252626D" type="text" id="bi" />
         </div>
 
@@ -23,24 +23,29 @@
         </div>
 
         <div class="form-group">
-          <label class="label-normal" for="password">Password</label>
+          <label class="label-normal" for="password">Senha</label>
           <input class="input-normal" placeholder="**************" type="password" id="password" required />
         </div>
 
         <div class="form-group">
-          <label class="label-normal" for="gender">Gender</label>
+          <label class="label-normal" for="gender">Genero</label>
           <select class="input-normal" id="gender" required>
             <option value="male">Masculino</option>
             <option value="female">Femenino</option>
           </select>
         </div>
 
-        <button class="btn-normal uppercase mb-5" type="submit">
-          Sign up
-        </button>
+        <div class="flex justify-between items-center">
+          <button class="btn-normal uppercase" type="submit">
+            Sign up
+          </button>
+          <a class="cursor-pointer font-semibold underline text-xl leading-none text-indigo-700" href="#" type="submit">
+            Possui uma conta? <br class="md:hidden" /><span class="">Logar &raquo;</span></a
+          >
+        </div>
       </form>
     </div>
-    <div class="bg-indigo-600 p-10 w-full md:h-screen flex flex-col justify-center items-center md:items-start">
+    <div class="bg-indigo-600 p-10 w-full md:h-screen flex flex-col justify-center md:items-start">
       <p class="text-4xl text-gray-200 font-bold leading-none md:text-6xl">
         Marque suas <br class="hidden md:inline" />
         consultas online!
