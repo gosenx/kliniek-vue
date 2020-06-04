9ad803feb972bbb1ebc271b3b0072c641b8ecdd4
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-06-04 17:36:21 +0200

    add input validation on the sigup form

--- src/layouts/Signup.vue
+++ src/layouts/Signup.vue
@@ -11,38 +11,72 @@
         <form action="">
           <div class="form-group">
             <label for="fullname">Nome</label>
-            <input type="text" class="input-normal" placeholder="Claudia Mirela Sandiru" id="fullname" />
-            <p class="hidden text-red-700 text-sm font-semibold">Campo obrigatorio!</p>
+            <input
+              type="text"
+              class="input-normal"
+              required
+              v-model="user.fullname"
+              placeholder="Claudia Mirela Sandiru"
+              id="fullname"
+            />
+            <p class="text-red-700 text-sm font-semibold" v-show="errors.fullname.length > 0">
+              {{ errors.fullname[0] }}
+            </p>
           </div>
 
           <div class="form-group">
             <label for="email">Email</label>
-            <input type="email" class="input-normal" placeholder="claudia.mirela@bobari.co.mz" id="email" />
-            <p class="hidden text-red-700 text-sm font-semibold">Campo obrigatorio!</p>
+            <input
+              type="email"
+              class="input-normal"
+              v-model="user.email"
+              required
+              placeholder="claudia.mirela@bobari.co.mz"
+              id="email"
+            />
+            <p class="text-red-700 text-sm font-semibold" v-show="errors.email.length > 0">
+              {{ errors.email[0] }}
+            </p>
           </div>
 
           <div class="form-group">
             <label for="gender">Genero</label>
-            <select id="gender" class="input-normal">
+            <select id="gender" required v-model="user.gender" class="input-normal">
               <option value="male">Masculino</option>
               <option value="female">Femenino</option>
             </select>
-            <p class="hidden text-red-700 text-sm font-semibold">Campo obrigatorio!</p>
+            <p class="text-red-700 text-sm font-semibold" v-show="errors.gender.length > 0">{{ errors.gender[0] }}</p>
           </div>
 
           <div class="form-group">
             <label for="password">Senha</label>
-            <input class="input-normal" type="password" placeholder="***************" />
-            <p class="hidden text-red-700 text-sm font-semibold">Campo obrigatorio!</p>
+            <input
+              class="input-normal"
+              required
+              v-model="user.password"
+              type="password"
+              placeholder="***************"
+            />
+            <p class="text-red-700 text-sm font-semibold" v-show="errors.password.length > 0">
+              {{ errors.password[0] }}
+            </p>
           </div>
 
           <div class="form-group">
             <label for="confirm-password">Confirmar senha</label>
-            <input class="input-normal" type="confirm-password" placeholder="***************" />
-            <p class="hidden text-red-700 text-sm font-semibold">Campo obrigatorio!</p>
+            <input
+              class="input-normal"
+              required
+              v-model="confirm_password"
+              type="password"
+              placeholder="***************"
+            />
+            <p class="text-red-700 text-sm font-semibold" v-show="errors.confirm_password.length > 0">
+              {{ errors.confirm_password[0] }}
+            </p>
           </div>
 
-          <button class="btn-normal">Registar</button>
+          <button class="btn-normal" @click.prevent="signup">Registar</button>
           <div class="w-full mt-2 text-gray-600">
             Já possui uma conta? <a class="text-blue-700 hover:underline" href="/login">&rArr;Entrar</a>
           </div>
@@ -53,26 +87,78 @@
 </template>
 
 <script>
-import http, { credentials } from "@/axios";
 export default {
   data() {
     return {
-      error: "",
-      user: { username: "pamossecuambe@gmail.com", password: "sysAdminPa$$" },
+      errors: {
+        fullname: [],
+        email: [],
+        gender: [],
+        password: [],
+        confirm_password: [],
+      },
+      user: {},
+      confirm_password: "",
     };
   },
   methods: {
-    testAxios() {
-      Object.assign(this.user, credentials);
-      http
-        .post("/oauth/token", this.user)
-        .then((response) => {
-          console.log(response.data);
-        })
-        .catch((err) => {
-          this.error = err;
-          console.log(err.response);
-        });
+    validateInput() {
+      this.errors.fullname = [];
+      this.errors.email = [];
+      this.errors.gender = [];
+      this.errors.password = [];
+      this.errors.confirm_password = [];
+
+      const matchEmail = /\S+@\S+\.\S+/;
+
+      if (!this.user.fullname) {
+        this.errors.fullname.push("Campo obrigatório!");
+      } else if (this.user.fullname.trim().split(" ").length <= 1) {
+        console.log(this.user.fullname.trim().split(" ").length);
+        this.errors.fullname.push("O nome deve ter no mínimo duas palavras.");
+      }
+
+      if (!this.user.email) {
+        this.errors.email.push("Campo obrigatório!");
+      } else {
+        if (!matchEmail.test(this.user.email)) {
+          this.errors.email.push("Formato de email inválido!");
+        }
+      }
+
+      if (!this.user.gender) {
+        this.errors.gender.push("Campo obrigatório!");
+      }
+
+      if (!this.user.password) {
+        this.errors.password.push("Campo obrigatório!");
+      } else if (this.user.password.length < 6) {
+        this.errors.password.push("A senha deve ter no mínimo 6 caracteres!");
+      }
+
+      if (!this.confirm_password) {
+        this.errors.confirm_password.push("Campo obrigatório!");
+      }
+
+      if (this.user.password && this.confirm_password && this.user.password !== this.confirm_password) {
+        this.errors.confirm_password.push("Este campo não coincide com a senha fornecida.");
+      }
+    },
+    noInputErrorsFound() {
+      if (
+        this.errors.fullname.length === 0 &&
+        this.errors.email.length === 0 &&
+        this.errors.gender.length === 0 &&
+        this.errors.password.length === 0 &&
+        this.errors.confirm_password.length === 0
+      )
+        return true;
+    },
+    signup() {
+      this.validateInput();
+      if (this.noInputErrorsFound()) {
+        console.log("The form is properly validated");
+      }
     },
   },
 };
