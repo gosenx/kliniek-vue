e9c3ac3c3f9d638d312c85fbd68139d9f70d2ff4
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2020-06-13 20:38:20 +0200

    rename layouts folder to pages

--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "./layouts/Home";
+import Home from "./pages/Home";
 import Example from "./components/Example";
-import Login from "./layouts/Login"
-import Signup from "./layouts/Signup";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 Vue.use(VueRouter);
 
